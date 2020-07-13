c3d4d3f15a87a4cf88623927935484890820ec92
张超 <zhang3611@qq.com>
2020-07-13 18:07:32 +0800

    sync

--- service/auth/generated.go
+++ service/auth/generated.go
@@ -45,6 +45,7 @@ type ComplexityRoot struct {
 	Mutation struct {
 		Login       func(childComplexity int, account string, password string) int
 		LoginStatus func(childComplexity int, token *string) int
+		Logout      func(childComplexity int, token *string) int
 	}
 
 	Query struct {
@@ -55,6 +56,7 @@ type ComplexityRoot struct {
 type MutationResolver interface {
 	Login(ctx context.Context, account string, password string) (string, error)
 	LoginStatus(ctx context.Context, token *string) (bool, error)
+	Logout(ctx context.Context, token *string) (bool, error)
 }
 type QueryResolver interface {
 	Hello(ctx context.Context) (*string, error)
@@ -98,6 +100,18 @@ func (e *executableSchema) Complexity(typeName, field string, childComplexity in
 		}
 
 		return e.complexity.Mutation.LoginStatus(childComplexity, args["token"].(*string)), true
+
+	case "Mutation.logout":
+		if e.complexity.Mutation.Logout == nil {
+			break
+		}
+
+		args, err := ec.field_Mutation_logout_args(context.TODO(), rawArgs)
+		if err != nil {
+			return 0, false
+		}
+
+		return e.complexity.Mutation.Logout(childComplexity, args["token"].(*string)), true
 
 	case "Query.hello":
 		if e.complexity.Query.Hello == nil {
@@ -190,7 +204,10 @@ type Query {
 type Mutation {
   "用户登录"
   login(account:String!,password:String!): String!
+  "登录状态查询"
   login_status(token:String): Boolean!
+  "登出"
+  logout(token: String):Boolean!
 }
 `, BuiltIn: false},
 }
@@ -245,6 +262,20 @@ func (ec *executionContext) field_Mutation_login_args(ctx context.Context, rawAr
 }
 
 func (ec *executionContext) field_Mutation_login_status_args(ctx context.Context, rawArgs map[string]interface{}) (map[string]interface{}, error) {
+	var err error
+	args := map[string]interface{}{}
+	var arg0 *string
+	if tmp, ok := rawArgs["token"]; ok {
+		arg0, err = ec.unmarshalOString2ᚖstring(ctx, tmp)
+		if err != nil {
+			return nil, err
+		}
+	}
+	args["token"] = arg0
+	return args, nil
+}
+
+func (ec *executionContext) field_Mutation_logout_args(ctx context.Context, rawArgs map[string]interface{}) (map[string]interface{}, error) {
 	var err error
 	args := map[string]interface{}{}
 	var arg0 *string
@@ -374,6 +405,47 @@ func (ec *executionContext) _Mutation_login_status(ctx context.Context, field gr
 	resTmp, err := ec.ResolverMiddleware(ctx, func(rctx context.Context) (interface{}, error) {
 		ctx = rctx // use context from middleware stack in children
 		return ec.resolvers.Mutation().LoginStatus(rctx, args["token"].(*string))
+	})
+	if err != nil {
+		ec.Error(ctx, err)
+		return graphql.Null
+	}
+	if resTmp == nil {
+		if !graphql.HasFieldError(ctx, fc) {
+			ec.Errorf(ctx, "must not be null")
+		}
+		return graphql.Null
+	}
+	res := resTmp.(bool)
+	fc.Result = res
+	return ec.marshalNBoolean2bool(ctx, field.Selections, res)
+}
+
+func (ec *executionContext) _Mutation_logout(ctx context.Context, field graphql.CollectedField) (ret graphql.Marshaler) {
+	defer func() {
+		if r := recover(); r != nil {
+			ec.Error(ctx, ec.Recover(ctx, r))
+			ret = graphql.Null
+		}
+	}()
+	fc := &graphql.FieldContext{
+		Object:   "Mutation",
+		Field:    field,
+		Args:     nil,
+		IsMethod: true,
+	}
+
+	ctx = graphql.WithFieldContext(ctx, fc)
+	rawArgs := field.ArgumentMap(ec.Variables)
+	args, err := ec.field_Mutation_logout_args(ctx, rawArgs)
+	if err != nil {
+		ec.Error(ctx, err)
+		return graphql.Null
+	}
+	fc.Args = args
+	resTmp, err := ec.ResolverMiddleware(ctx, func(rctx context.Context) (interface{}, error) {
+		ctx = rctx // use context from middleware stack in children
+		return ec.resolvers.Mutation().Logout(rctx, args["token"].(*string))
 	})
 	if err != nil {
 		ec.Error(ctx, err)
@@ -1575,6 +1647,11 @@ func (ec *executionContext) _Mutation(ctx context.Context, sel ast.SelectionSet)
 			}
 		case "login_status":
 			out.Values[i] = ec._Mutation_login_status(ctx, field)
+			if out.Values[i] == graphql.Null {
+				invalids++
+			}
+		case "logout":
+			out.Values[i] = ec._Mutation_logout(ctx, field)
 			if out.Values[i] == graphql.Null {
 				invalids++
 			}
