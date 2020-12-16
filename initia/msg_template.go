d454df3cb62ef439e4ac6766bf3ebfa5733f1cd1
zhanghup <zhang3611@qq.com>
2020-12-16 22:08:20 +0800

    sync

--- initia/msg_template.go
+++ initia/msg_template.go
@@ -8,35 +8,34 @@ import (
 )
 
 func InitMsgTemplate(db *xorm.Engine) {
-	InitMsgTemplateCode(db, "system")
+	InitMsgTemplateCode(db, "系统消息", "system", "notice", "1", "web", "系统消息")
 }
 
-func InitMsgTemplateCode(db *xorm.Engine, codes ...string) {
-	for _, code := range codes {
-		ok, err := db.Table(beans.MsgTemplate{}).Where("code = ?", code).Exist()
+func InitMsgTemplateCode(db *xorm.Engine, name, code, typ, level, target, remark string) {
+	ok, err := db.Table(beans.MsgTemplate{}).Where("code = ?", code).Exist()
+	if err != nil {
+		tog.Error(err.Error())
+		return
+	}
+	if !ok {
+		_, err = db.Insert(beans.MsgTemplate{
+			Bean: beans.Bean{
+				Id:     tools.Ptr.Uid(),
+				Status: tools.Ptr.String("1"),
+			},
+			Name:   &name,
+			Code:   &code,
+			Type:   &typ,
+			Level:  &level,
+			Target: &target,
+			Remark: &remark,
+			Delay:  tools.Ptr.Int64(5),
+			Alert:  tools.Ptr.Int64(86400), // 提前一天开始提醒
+			Expire: tools.Ptr.Int64(86400), // 消息推送后一天未确认过期
+		})
 		if err != nil {
 			tog.Error(err.Error())
-			continue
+			return
 		}
-		if !ok {
-			_, err = db.Insert(beans.MsgTemplate{
-				Bean: beans.Bean{
-					Id:     tools.Ptr.Uid(),
-					Status: tools.Ptr.String("1"),
-				},
-				Name:        tools.Ptr.String("系统消息"),
-				Code:        &code,
-				Type:        tools.Ptr.String("message"),
-				Level:       tools.Ptr.String("3"),
-				Target:      tools.Ptr.String("web"),
-				Expire:      nil,
-				Remark:      tools.Ptr.String("系统消息"),
-			})
-			if err != nil {
-				tog.Error(err.Error())
-				continue
-			}
-		}
-
 	}
 }
