eb7247b7558d92bda9d0e88afd2fa19e6c1afdc4
zhanghup <zhang3611@qq.com>
2020-12-19 15:09:40 +0800

    sync

--- initia/msg_template.go
+++ initia/msg_template.go
@@ -8,10 +8,10 @@ import (
 )
 
 func InitMsgTemplate(db *xorm.Engine) {
-	InitMsgTemplateCode(db, "系统消息", "system", "notice", "1", "web", "系统消息")
+	InitMsgTemplateCode(db, "系统消息", "system", "notice", "1", "web", "系统消息", "")
 }
 
-func InitMsgTemplateCode(db *xorm.Engine, name, code, typ, level, target, remark string) {
+func InitMsgTemplateCode(db *xorm.Engine, name, code, typ, level, target, remark, templateCode string) {
 	ok, err := db.Table(beans.MsgTemplate{}).Where("code = ?", code).Exist()
 	if err != nil {
 		tog.Error(err.Error())
@@ -23,15 +23,16 @@ func InitMsgTemplateCode(db *xorm.Engine, name, code, typ, level, target, remark
 				Id:     tools.Ptr.Uid(),
 				Status: tools.Ptr.String("1"),
 			},
-			Name:   &name,
-			Code:   &code,
-			Type:   &typ,
-			Level:  &level,
-			Target: &target,
-			Remark: &remark,
-			Delay:  tools.Ptr.Int64(5),
-			Alert:  tools.Ptr.Int64(86400), // 提前一天开始提醒
-			Expire: tools.Ptr.Int64(86400), // 消息推送后一天未确认过期
+			Name:         &name,
+			Code:         &code,
+			Type:         &typ,
+			Level:        &level,
+			Target:       &target,
+			Remark:       &remark,
+			Delay:        tools.Ptr.Int64(5),
+			Alert:        tools.Ptr.Int64(86400), // 提前一天开始提醒
+			Expire:       tools.Ptr.Int64(86400), // 消息推送后一天未确认过期
+			TemplateCode: &templateCode,
 		})
 		if err != nil {
 			tog.Error(err.Error())
